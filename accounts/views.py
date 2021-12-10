ef99706e8c673901287bab2d8aa7643611a7f44b
Akira Miyamoto <64848070+miyaTb@users.noreply.github.com>
2021-12-10 20:37:46 +0900

    ログイン機能実装 ログイン後はindexに飛ぶ

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def home(request):
+    return render(request, 'accounts/home.html')
+
+@login_required
+def private_page(request):
+    return render(request, 'accounts/private.html', {})
+
+def public_page(request):
+    return render(request, 'accounts/public.html', {})
