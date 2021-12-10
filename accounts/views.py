3a2ce296cc5ff494971eadc91c2256c35479f513
Akira Miyamoto <64848070+miyaTb@users.noreply.github.com>
2021-12-10 21:05:03 +0900

    signup実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
     return render(request, 'accounts/home.html')
 
-@login_required
-def private_page(request):
-    return render(request, 'accounts/private.html', {})
+#@login_required
+class SignUp(CreateView):
+    form_class = SignUpForm
+    template_name = "accounts/signup.html" 
+    success_url = reverse_lazy('index')
 
-def public_page(request):
-    return render(request, 'accounts/public.html', {})
+    def form_valid(self, form):
+        user = form.save() # formの情報を保存
+        login(self.request, user) # 認証
+        self.object = user 
+        return HttpResponseRedirect(self.get_success_url())
--- eng_team/urls.py
+++ eng_team/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/',accounts_views.SignUp.as_view(), name='signup'),
 ]
